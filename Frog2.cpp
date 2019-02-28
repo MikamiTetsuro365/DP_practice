3b2f28d2c24cf91ead68db2c13bf7d5fc0e77259
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-03-01 08:40:18 +0900

    upd

---
+++ Frog2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <string>
+#include <cmath>
+
+using namespace std;
+//int moreGcd
+int N = 0;
+int K = 0;
+vector<int > h;
+
+int main(){
+    
+    cin >> N;
+    cin >> K;
+
+    int t = 0;
+
+     h.push_back(0);
+    for(int i = 0; i < N; i++){
+        cin >> t;
+        h.push_back(t);
+    }
+    
+    vector<int > cost(N, 11451419);
+
+    //初期化的な
+    cost[0] = 0;
+    cost[1] = 0;
+
+    for(int i = 1; i <= N; i++){
+        cout << h[i] << endl;
+        for(int j = 1; j <= K; j++){
+            if(i - j >= 1){
+                //cout << abs(h[i]-h[i-j]) << endl;
+                
+                cost[i] = min(cost[i], cost[i-j] + abs(h[i]-h[i-j]));
+                cout << cost[i] << " " ;
+
+            }else{
+                break;
+            }
+        
+        }
+        
+        cout << "::" << cost[i] << endl;
+
+    }
+
+    cout << cost[N] << endl;
+
+}
