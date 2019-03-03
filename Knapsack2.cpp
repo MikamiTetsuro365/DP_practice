2dbc3ae34ae769fc6d98dd917208d014458ecb45
MikamiTetsuro365 <sima.tetteke365@gmail.com>
2019-03-03 18:24:26 +0900

    ?

--- Knapsack2.cpp
+++ Knapsack2.cpp
@@ -18,45 +18,43 @@ int main(){
     cin >> W;
 
     vector<pair<ll, ll> > T;
-    //価値：重量 
-    //T = vector<vector<ll> >(N + 1, vector<ll>(2, 0));
-    //初期化
-    //T[0][0] = 0;
-    //T[0][1] = 0;
 
     ll value = 0;
     ll weight = 0;
-
+    ll max_value = 0;
+    
+    //入力
     for(int i = 0; i < N; i++){
-         cin >> weight >> value;
-         T.push_back(make_pair(value, weight));
+        cin >> weight >> value;
+        max_value += value;
+        T.push_back(make_pair(value, weight));
     }
 
-    //価値が大きい順にソート
+    //価値が大きい順にソート 
     sort(T.rbegin(), T.rend());
     //余白
-    T.insert(T.begin(), make_pair(value, weight));
+    T.insert(T.begin(), make_pair(0, 0));
 
-    //dp
+    //dp　Value　価値を基準に表を作る
     vector<vector<ll> > dp;
-    dp = vector<vector<ll> >(N + 1, vector<ll>(W + 1, -1));
+    dp = vector<vector<ll> >(N + 1, vector<ll>(max_value + 1, -1));
     dp[0][0] = 0;
 
     //答え
     ll ans = 0;
 
     for(int i = 1; i <= N; i++){
-        for(int j = 0; j <= W; j++){
+        for(int j = 0; j <= max_value; j++){
             if(dp[i - 1][j] > -1){
                 //直下を更新
                 if(dp[i][j] <= dp[i - 1][j]){
                     dp[i][j] = dp[i - 1][j];
                 }
                 //荷物を足し合わせたところを更新
-                if(dp[i][j + T[i].second] <= dp[i - 1][j] + T[i].first && j + T[i].second <= W){
-                    dp[i][j +T[i].second] = dp[i - 1][j] + T[i].first;
+                if(dp[i][j + T[i].first] >= dp[i - 1][j] + T[i].second && dp[i - 1][j] + T[i].second <= W){
+                    dp[i][j +T[i].first] = dp[i - 1][j] + T[i].second;
                     //ans確認
-                    if(ans < dp[i - 1][j] + T[i].first) ans = dp[i - 1][j] + T[i].first;
+                    if(ans < j + T[i].first) ans = j + T[i].first;
                 }
             }          
         }
@@ -67,15 +65,12 @@ int main(){
 
     //確認
     
-    /*
-    for(int i = 0; i < N + 1; i++){
-        for(int j = 0; j < W + 1; j++){
+    
+    for(int i = 0; i <= N; i++){
+        for(int j = 0; j <= max_value; j++){
             cout << dp[i][j] << " ";
         }
         cout << endl;
     }
-    */
     
-    
-
 }
